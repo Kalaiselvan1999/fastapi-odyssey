6dbbd1e88aa526f6dcbb4cf174172330539f2557
kalaiselvan <selvankalai638@gmal.com>
2024-04-15 22:45:18 +0530

    added celery

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
-
 from dotenv import load_dotenv, find_dotenv
+from celery import Celery
 
 load_dotenv(find_dotenv())
 
@@ -12,3 +12,11 @@ DB_PORT = os.environ.get('DB_PORT')
 
 SENDER_MAIL_ID = os.environ.get('SENDER_MAIL_ID')
 SENDER_MAIL_PASSWORD = os.environ.get('SENDER_MAIL_PASSWORD')
+
+app = Celery(broker="amqp://guest:guest@localhost:5672/", include=['auth.tasks'])
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
